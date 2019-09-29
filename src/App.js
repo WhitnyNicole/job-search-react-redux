c0838296afb32bec75c6010e07f0e9410e1c7c08
Whitny Edwards <wne4e2@gmail.com>
2019-09-29 18:10:58 -0400

    set up routes, add all necessary imports and add componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './components/Home';
+import ReviewsList from './containers/ReviewsList';
+import NewReview from './components/NewReview'
+import { connect } from 'react-redux'
+import { getReviews } from './redux/actions/reviews'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    this.props.getReviews();
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/reviews" component={ReviewsList} />
+          <Route exact path="/reviews/new" component={NewReview} />
+        </Router>
+      </div>
+    )
+  };
 }
 
-export default App;
+export default connect(
+  null, 
+    { getReviews }
+  )(App);
