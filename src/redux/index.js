5973d2b23ad7494d151a5683453cc39a801baa62
Whitny Edwards <wne4e2@gmail.com>
2019-10-05 17:33:26 -0400

    include user reducer

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,10 +2,12 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import interviews from './reducers/interviews';
 import reviews from './reducers/reviews';
+import users from './reducers/users';
 
 const reducer = combineReducers({
     interviews,
-    reviews
+    reviews,
+    users
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
