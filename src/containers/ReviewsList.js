d063b6e226922727eae238763b1a93abb9ae7831
Whitny Edwards <wne4e2@gmail.com>
2019-10-05 15:45:14 -0400

    add reviews list that displays all reviews

---
+++ src/containers/ReviewsList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class ReviewsList extends Component {
+
+    render() {
+        if (this.props.reviews.length == 0) {
+            return <p>Loading...</p>
+        }
+        return (
+            <div>
+                <h1> Reviews List </h1>
+                {this.props.reviews.map(review => (
+                    <p>{review.inquiry}</p>
+                ))}
+            </div>
+        )      
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        reviews: state
+    }
+}
+
+export default connect(mapStateToProps)(ReviewsList);
