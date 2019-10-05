3839e2fd68ce80996c5c7047358b088ab6cc0450
Whitny Edwards <wne4e2@gmail.com>
2019-10-05 15:45:59 -0400

    add reviews actions file which sends fetch request to rails backend

---
+++ src/redux/actions/reviews.js
@@ -0,0 +1,35 @@
+const fetchReviews = (reviews) => {
+    return {
+        type: 'FETCH_REVIEWS_SUCCESS', 
+        reviews  
+    }
+}
+
+export const getReviews = () => {
+    return function(dispatch) {
+    return fetch(`http://localhost:3001/api/v1/reviews`)
+        .then(res => res.json())
+        .then(reviews => dispatch(fetchReviews(reviews)))
+    };
+};
+
+export const createReview = (reviewObject) => {
+    const reviewToCreate = { review: reviewObject };
+    return dispatch => {
+            fetch(`http://localhost:3001/api/v1/reviews`, {
+                method: 'POST',
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({reviewToCreate})
+            })
+            .then(res => res.json())
+            .then(review => 
+                dispatch({
+                    type: 'REVIEW_CREATE_SUCCESS', 
+                    payload: review
+                })
+            )
+        }
+}
