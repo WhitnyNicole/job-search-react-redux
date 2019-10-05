f9ab0b48212da5d5c6bebbd10a07b40f9f868e97
Whitny Edwards <wne4e2@gmail.com>
2019-10-05 15:47:44 -0400

    work on updating to combine reducers

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,7 +1,12 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import interviewsReducer from './reducers/interviews'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
+import interviewsReducer from './reducers/interviews';
+import reviewsReducer from './reducers/reviews';
 
+// const reducer = combineReducers({
+//     interviews: interviewsReducer,
+//     reviews: reviewsReducer 
+// })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 let store = createStore(interviewsReducer, composeEnhancer(applyMiddleware(thunk)));
