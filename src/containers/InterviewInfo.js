27ee0f9d253c75adeed29099427f4dc670c5935c
Whitny Edwards <wne4e2@gmail.com>
2019-10-06 16:47:54 -0400

    add interviewinfo component to render interview information

---
+++ src/containers/InterviewInfo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const InterviewInfo = ({ interview }) => {
+    return (
+        interview ?
+            <div>
+                <h2>{interview.company_name}</h2>
+                <p>{interview.location}</p>
+                <p>{interview.position}</p>
+            </div> :
+            <p>No interview info yet! </p>
+    )
+}
+
+export default InterviewInfo
