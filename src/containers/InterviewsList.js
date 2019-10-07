161da03f96a144d763a1869301fe4b5e91d142bb
Whitny Edwards <wne4e2@gmail.com>
2019-10-07 19:56:16 -0400

    add route for individual interview, import Route

--- src/containers/InterviewsList.js
+++ src/containers/InterviewsList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, Route } from 'react-router-dom'
 import InterviewInfo from './InterviewInfo.js'
 
 class InterviewsList extends Component {
@@ -12,9 +12,9 @@ class InterviewsList extends Component {
         return (
             <div>
                 <h1> Interviews List </h1>
-                {this.props.interviews.map(interview => (<p key={interview.id}><Link to={`/interviews/${interview.id}`}>{interview.company_name}: {interview.position}</Link><InterviewInfo interview={
-                    interview} /></p>
+                {this.props.interviews.map(interview => (<p key={interview.id}><Link to={`/interviews/${interview.id}`}>{interview.company_name}: {interview.position}</Link></p>
                 ))}
+        <Route path={`${this.props.match.url}/:interviewId`} render={(routerProps) => <InterviewInfo {...routerProps}/>}/>
             </div>
         )      
     }
