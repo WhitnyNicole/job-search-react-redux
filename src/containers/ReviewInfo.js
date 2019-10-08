a73b19ada31045c5fa139126932b85485811929d
Whitny Edwards <wne4e2@gmail.com>
2019-10-07 21:34:16 -0400

    add review info container component

---
+++ src/containers/ReviewInfo.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+// const ReviewInfo = ({ review }) => {
+//     return (
+//         interview ?
+//             <div>
+//                 <h2>{review.inquiry}</h2>
+//                 <p>{review.answer}</p>
+//             </div> :
+//             <p>No review info yet! </p>
+//     )
+// }
+
+const ReviewInfo = (props) => {
+    debugger
+    return (
+        <div>
+            <p>No Review info yet! </p>
+        </div>
+    )
+}
+
+export default ReviewInfo
+
+// props.match.params.id
