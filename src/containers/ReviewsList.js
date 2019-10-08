6b7d1a4516ca2ca03955cdce1d22a16142a9efde
Whitny Edwards <wne4e2@gmail.com>
2019-10-07 21:33:52 -0400

    add link to individual review

--- src/containers/ReviewsList.js
+++ src/containers/ReviewsList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, Route } from 'react-router-dom'
+import ReviewInfo from  './ReviewInfo.js'
+
 class ReviewsList extends Component {
 
     render() {
@@ -12,6 +14,7 @@ class ReviewsList extends Component {
                 <h1> Reviews List </h1>
                 {this.props.reviews.map(review => (<p key={review.id}><Link to={`/reviews/${review.id}`}>{review.interview.company_name}:{review.inquiry}</Link></p>
                 ))}
+                <Route path={`${this.props.match.url}/:reviewId`} render={(routerProps) => <ReviewInfo {...routerProps}/>}/>
             </div>
         )      
     }
