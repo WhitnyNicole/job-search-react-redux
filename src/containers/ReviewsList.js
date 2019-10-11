833a5198788f41e018de7a01dc483c9918cca879
Whitny Edwards <wne4e2@gmail.com>
2019-10-11 17:52:05 -0400

    refactor to include reviewsReducer in mapStateToProps and update render function

--- src/containers/ReviewsList.js
+++ src/containers/ReviewsList.js
@@ -5,17 +5,22 @@ import { Link } from 'react-router-dom'
 class ReviewsList extends Component {
 
     render() {
+        let reviewsList;
         if (this.props.reviews.length === 0) {
             return <p>Loading...</p>
+        } else{
+            reviewsList = this.props.reviews.map(review => {
+               return ( <p key={review.id}>
+                    <Link to={`/reviews/${review.id}`}>{review.interview.company_name} - {review.inquiry}</Link>
+                </p>
+               )
+            })
         }
+
         return (
             <div>
-                <h1> Review List</h1>
-                {this.props.reviews.map(review => (
-                    <p key={review.id}>
-                        <Link to={`/reviews/${review.id}`}>{review.interview.company_name} - {review.inquiry}</Link>
-                    </p>
-                ))}
+                <h1> Reviews List</h1>
+                {reviewsList}
             </div>
         )   
     }
@@ -23,7 +28,7 @@ class ReviewsList extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        reviews: state.reviews
+        reviews: state.reviewsReducer.reviews
     }
 }
 
