feecd7b8fe30e8e4d57a4e5bb0916c656543cc3b
Whitny Edwards <wne4e2@gmail.com>
2019-10-10 21:53:32 -0400

    refactor

--- src/containers/InterviewsList.js
+++ src/containers/InterviewsList.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Link, Route } from 'react-router-dom'
-import InterviewInfo from './InterviewInfo.js'
+import { Link } from 'react-router-dom'
 
 class InterviewsList extends Component {
 
@@ -9,14 +8,17 @@ class InterviewsList extends Component {
         if (this.props.interviews.length === 0) {
             return <p>Loading...</p>
         }
+       
         return (
             <div>
-                <h1> Interviews List </h1>
-                {this.props.interviews.map(interview => (<p key={interview.id}><Link to={`/interviews/${interview.id}`}>{interview.company_name}: {interview.position}</Link></p>
+                <h1> Interview List</h1>
+                {this.props.interviews.map(interview => (
+                    <p key={interview.id}>
+                        <Link to={`/interviews/${interview.id}`}>{interview.company_name}</Link>
+                    </p>
                 ))}
-                <Route path={`${this.props.match.url}/:interviewId`} render={(routerProps) => <InterviewInfo interviews={this.props.interviews} {...routerProps}/>}/>
             </div>
-        )      
+        )   
     }
 }
 
--- src/containers/ReviewsList.js
+++ src/containers/ReviewsList.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Link, Route } from 'react-router-dom'
-import ReviewInfo from  './ReviewInfo.js'
+import { Link } from 'react-router-dom'
 
 class ReviewsList extends Component {
 
@@ -11,12 +10,14 @@ class ReviewsList extends Component {
         }
         return (
             <div>
-                <h1> Reviews List </h1>
-                {this.props.reviews.map(review => (<p key={review.id}><Link to={`/reviews/${review.id}`}>{review.interview.company_name}:{review.inquiry}</Link></p>
+                <h1> Review List</h1>
+                {this.props.reviews.map(review => (
+                    <p key={review.id}>
+                        <Link to={`/reviews/${review.id}`}>{review.inquiry}</Link>
+                    </p>
                 ))}
-                <Route path={`${this.props.match.url}/:reviewId`} render={(routerProps) => <ReviewInfo reviews={this.props.reviews}  {...routerProps}/>}/>
             </div>
-        )      
+        )   
     }
 }
 
