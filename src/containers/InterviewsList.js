0746d5dca019cd7d30762fcc1b5a5776bf27c56c
Whitny Edwards <wne4e2@gmail.com>
2019-10-11 17:48:14 -0400

    refactor to include interviewsReducer in mapStateToProps and update render function

--- src/containers/InterviewsList.js
+++ src/containers/InterviewsList.js
@@ -5,18 +5,22 @@ import { Link } from 'react-router-dom'
 class InterviewsList extends Component {
 
     render() {
+        let interviewsList;
         if (this.props.interviews.length === 0) {
             return <p>Loading...</p>
+        } else{
+            interviewsList = this.props.interviews.map(interview => {
+               return ( <p key={interview.id}>
+                    <Link to={`/interviews/${interview.id}`}>{interview.company_name}</Link>
+                </p>
+               )
+            })
         }
-       
+       console.log(interviewsList)
         return (
             <div>
                 <h1> Interview List</h1>
-                {this.props.interviews.map(interview => (
-                    <p key={interview.id}>
-                        <Link to={`/interviews/${interview.id}`}>{interview.company_name}</Link>
-                    </p>
-                ))}
+                {interviewsList}
             </div>
         )   
     }
@@ -24,7 +28,7 @@ class InterviewsList extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        interviews: state.interviews
+        interviews: state.interviewsReducer.interviews
     }
 }
 
